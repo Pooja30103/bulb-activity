b631445b2ae9b05306b1243b15c31af377b45c5d
Pooja30103 <138848860+Pooja30103@users.noreply.github.com>
2023-07-31 16:23:07 +0530

    Add files via upload

---
+++ two.js
@@ -0,0 +1,7 @@
+function on(){
+    document.getElementById("img").src="https://toppng.com/public/uploads/preview/light-bulb-on-off-png-11553940319kdxsp3rf0i.png"
+}
+function off()
+{
+    document.getElementById("img").src="https://toppng.com/public/uploads/preview/light-bulb-on-off-png-11553940208oq66nq8jew.png " 
+}
